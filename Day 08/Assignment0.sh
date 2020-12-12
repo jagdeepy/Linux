a29664e7b8836e2ba9567f73e691fcbd60c67871
jagdeepy <54890288+jagdeepy@users.noreply.github.com>
2020-12-13 01:51:05 +0530

    Add files via upload

---
+++ Day 08/Assignment0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# example of using arguments to a script
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+filename="/home//Desktop/LetsUpgrade/Linux/Day 08$ /$today.ponies"
+echo $filename;
+ 
+echo "Your username : $(echo $USER)"
+ 
+echo "Current date and time : $(date)"
+ 
+echo "Currently logged on users:"
+
